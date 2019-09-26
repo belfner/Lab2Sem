4f5a9a4f9c9f10de66a54add4a5d3d4a1d4dcec2
belfner <ben.elfner10@gmail.com>
2019-09-26 11:44:07 -0400

    More Simple and Faster

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
 
-    public static long startTime;
-    public static DecimalFormat df = new DecimalFormat("#.###");
+        public static long startTime;
+        public static DecimalFormat df = new DecimalFormat("#.###");
 
     public static void main (String args[]) {
         System.out.println("Starting...");
--- src/Monitor.java
+++ src/Monitor.java
@@ -1,55 +1,51 @@
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+
 
 class Monitor
 {
     String name;
-    boolean waiting;
     Semaphore sem;
 
-    public Monitor(final String name,Semaphore sem)
+    public Monitor(final String name, Semaphore sem)
     {
         this.name = name;
         this.sem = sem;
     }
 
-    public synchronized void ping(final Monitor Monitor)
+    public synchronized void ping(final Monitor m)
     {
-        if(!sem.tryAcquire())
-        {
 
+        while (!sem.tryAcquire())
+        {
+            System.out.println(this.name + " (ping): waiting for permit at " + Main.getTime());
             try
             {
-                System.out.println(this.name + " (ping): wait to ask to confirm at "+ Main.getTime());
                 this.wait();
-                sem.acquire();
-            } catch (InterruptedException exc)
+            } catch (InterruptedException e)
             {
-                System.out.println(exc);
+                e.printStackTrace();
             }
         }
-        else
-        {
-            System.out.println(this.name + "(Semaphore): acquired permit at "+ Main.getTime());
-        }
 
-        System.out.println(this.name + "(ping): asking " + Monitor.name + " to confirm at "+ Main.getTime());
-        Monitor.confirm(this);
-        System.out.println(this.name + "(ping): got confirmation at "+ Main.getTime());
+        System.out.println(this.name + "(Semaphore): acquired permit at " + Main.getTime());
+
+        System.out.println(this.name + "(ping): asking " + m.name + " to confirm at " + Main.getTime());
+        m.confirm(this);
+        System.out.println(this.name + "(ping): got confirmation at " + Main.getTime());
         sem.release();
-        System.out.println(this.name + "(Semaphore): released permit at "+ Main.getTime());
-        Monitor.release(this);
+        System.out.println(this.name + "(Semaphore): released permit at " + Main.getTime());
+        m.release(this);
 
     }
 
-    public synchronized void confirm(final Monitor Monitor)
+    public synchronized void confirm(final Monitor m)
     {
-        System.out.println(this.name + "(confirm): confirm to " + Monitor.name+ " at "+ Main.getTime());
+        System.out.println(this.name + "(confirm): confirm to " + m.name + " at " + Main.getTime());
     }
 
-    public synchronized void release(final Monitor Monitor)
+    public synchronized void release(final Monitor m)
     {
-        System.out.println(Monitor.name + ": " + this.name + " released at "+ Main.getTime());
+        System.out.println(m.name + ": " + this.name + " released at " + Main.getTime());
         this.notify();
     }
 
